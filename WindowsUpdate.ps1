f7338802eae80cf6c6ee778bfc4d02206a536ce1
Red5 <115599129+ateeBBS@users.noreply.github.com>
2023-07-23 11:38:35 +0930

    Update WindowsUpdate.ps1

--- WindowsUpdate.ps1
+++ WindowsUpdate.ps1
@@ -5,6 +5,37 @@
 # Schedule task parameters
 # PowerShell.exe
 # -windowstyle hidden C:\Scripts\Automated_Windows_Updates.ps1
+# Execute Script as Administrator
+Function Check-RunAsAdministrator()
+{
+  #Get current user context
+  $CurrentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+  
+  #Check user is running the script is member of Administrator Group
+  if($CurrentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator))
+  {
+       Write-host "Script is running with Administrator privileges!"
+  }
+  else
+    {
+       #Create a new Elevated process to Start PowerShell
+       $ElevatedProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell";
+ 
+       # Specify the current script path and name as a parameter
+       $ElevatedProcess.Arguments = "& '" + $script:MyInvocation.MyCommand.Path + "'"
+ 
+       #Set the Process to elevated
+       $ElevatedProcess.Verb = "runas"
+ 
+       #Start the new elevated process
+       [System.Diagnostics.Process]::Start($ElevatedProcess)
+ 
+       #Exit from the current, unelevated, process
+       Exit
+ 
+    }
+}
+
 
 # Script to automatically install Windows updates
 Function InstallWindowsModules
